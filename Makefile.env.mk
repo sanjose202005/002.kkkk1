82a8a05413db6c363bf1f95b974fcb32f8bcb2d6
dyn <y@e>
2020-06-23 21:15:26 -0700

    20200623_2115PM_

--- Makefile.env.mk
+++ Makefile.env.mk
@@ -12,29 +12,3 @@ httpproxy2_main := \
 	\
 	httpproxy2_main
 
-# https://medium.com/@mlowicki/http-s-proxy-in-golang-in-less-than-100-lines-of-code-6a51c2f2c38c
-sslConfig:=/etc/ssl/openssl.cnf
-ssl_conf:
-	[ -d ssl ] || mkdir ssl
-	[ -f ssl/myOpenssl.conf ] \
-		&& printf '\nAlread found : ssl/myOpenssl.conf , skip. \n\n' \
-		|| ( \
-		cat $(sslConfig)                              > ssl/myOpenssl.conf && \
-		printf '[SAN]\nsubjectAltName=DNS:localhost' >> ssl/myOpenssl.conf \
-		)
-	[ -f ssl/myServer.key ] \
-		&& printf '\nAlread found : ssl/myServer.key , skip. \n\n' \
-		|| \
-		openssl req \
-		-newkey rsa:2048 \
-		-x509 \
-		-nodes \
-		-keyout ssl/myServer.key \
-		-new \
-		-out ssl/myServer.pem \
-		-subj /CN=localhost \
-		-reqexts SAN \
-		-extensions SAN \
-		-config                 ssl/myOpenssl.conf    \
-		-sha256 \
-		-days 3650
